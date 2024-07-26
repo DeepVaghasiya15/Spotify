593e36fd2dc25b06a2cd04be7858f690c13b9fbc
Deep <deepvaghasiya15@gmail.com>
2024-07-26 13:18:10 +0530

    Created Splash Screen

---
+++ lib/core/configs/assets/app_vector.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppVectors{
+
+  static const String basePath = 'assets/vectors/';
+  static const String format = '.svg';
+
+  static const String logo = '${basePath}spotify_logo$format';
+}
